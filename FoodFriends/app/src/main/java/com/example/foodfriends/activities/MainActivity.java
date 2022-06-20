7d8b66ed5415a18304161ade05d4acbf811808ad
Ola Okoroafor <olaokoroafor@fb.com>
2022-06-20 16:10:00 -0700

    bottom navigation view completed

--- FoodFriends/app/src/main/java/com/example/foodfriends/activities/MainActivity.java
+++ FoodFriends/app/src/main/java/com/example/foodfriends/activities/MainActivity.java
@@ -1,22 +1,41 @@
 package com.example.foodfriends.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.foodfriends.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main Activity";
     Button btnLogOut;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        //final Fragment timelineFragment = new TimelineFragment();
+        //final Fragment composeFragment = new ComposeFragment();
+        //final Fragment profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
         btnLogOut = findViewById(R.id.btnLogOut);
         btnLogOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,5 +47,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Log.i(TAG, "Menu item selected");
+                //Fragment fragment = timelineFragment;
+                if (item.getItemId() == R.id.menu_explore){
+                    Log.i(TAG, "Explore Fragment");
+                    //fragment = timelineFragment;
+                }
+                if (item.getItemId() == R.id.menu_search){
+                    Log.i(TAG, "Search selected");
+                    //fragment = composeFragment;
+                }
+                if (item.getItemId() == R.id.menu_profile){
+                    //compose action has been selected
+                    //showComposeFragment("");
+                    Log.i(TAG, "profile selected");
+                    //fragment = profileFragment;
+                    /*
+                    Bundle bundle = new Bundle();
+                    bundle.putParcelable("user", ParseUser.getCurrentUser());
+                    fragment.setArguments(bundle);
+                    */
+                }
+                /*
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.tvPlaceholder, fragment);
+                transaction.addToBackStack(null).commit();
+                */
+                return true;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.menu_explore);
     }
 }
