6ae6c50d58139ab25244c2d99c80b5f15f0cde3d
Ola Okoroafor <olaokoroafor@fb.com>
2022-07-07 16:46:37 -0700

    better function description

--- FoodFriends/app/src/main/java/com/example/foodfriends/activities/MapActivity.java
+++ FoodFriends/app/src/main/java/com/example/foodfriends/activities/MapActivity.java
@@ -49,8 +49,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     /**
      * Manipulates the map when it's available.
      * The API invokes this callback when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * Added markers for all the restaurants and made them clickable to go to restaurant detal screen
      * If Google Play services is not installed on the device, the user receives a prompt to install
      * Play services inside the SupportMapFragment. The API invokes this method after the user has
      * installed Google Play services and returned to the app.
